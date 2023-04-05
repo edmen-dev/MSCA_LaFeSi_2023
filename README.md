72262501266f1337f18bed68f5ba0f3b7b2097d5
edmen-dev <emendivetapia@gmail.com>
2023-04-05 17:15:35 +0200

    Update general metadata and README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Licensing described in LICENSE files:
   - Python code found in folder `hdf5_and_metadata_class`: **MIT license**
-  - Data in folder `calculation`: **Creative Commons Attribution 4.0 International**
+  - Datasets in folder `calculation`: **Creative Commons Attribution 4.0 International**
 
 Authorship and publication:
   - **Main author:** Eduardo Mendive Tapia
@@ -9,10 +9,12 @@ Authorship and publication:
 
 ---
 
-This notebook shows how to access and manipulate the most important data stored in this repository:
- 1. `Weiss field` and `density of states` data are stored in marmot.hdf5
- 2. The folder `calculation` contains all inputs and outputs, produced by `marmot code` (https://warwick.ac.uk/fac/sci/physics/research/theory/research/electrstr/marmot/).
- 3. Metadata is described in `marmot.json` file.
+This repository contains the data generated and used for the work published in https://arxiv.org/abs/2302.06484. The notebook `notebook.ipynb` shows how to access and manipulate the most important data stored in this repository:
+ 1. `Weiss field` and `density of states` datasets are stored in marmot.hdf5
+ 2. The folder `calculation` contains all inputs and outputs, produced by `marmot code` (https://warwick.ac.uk/fac/sci/physics/research/theory/research/electrstr/marmot/). Inside there is a list of folders corresponding to different values of the lattice parameter for the DFT calculation. Each one of these folders contains another set of folders:
+       - **Hutsepot**: Contains an initial DFT calculation using `hutsepot code` to generate input potentials.
+       - **m.0...**: Contains inputs and outputs of a calculation made by `marmot code` for specific values of the magnetic order parameter ***m***.
+ 3. For further information on this repository, metadata is described in `marmot.json` file.
  4. The folder `hdf5_and_metadata_class` contains a python class called by `set_hdf5.ipynb` used to create:
     - `marmot.hdf5`
     - `marmot.json`
--- hdf5_and_metadata_class/metadata_general.py
+++ hdf5_and_metadata_class/metadata_general.py
@@ -15,14 +15,18 @@ class metadata_general:
             "access_conditions": {
                 "description": "Conditions under which access is given if record is restricted.",
                 "title": "Access conditions",
-                "type": "The access to the data in this repository is given upon request to the main author."
+                "type": "The access to the data in this repository is described in LICENSE files."
             },
-            "access_right": {
-                "default": "Upon request",
-                "description": "email for request: e.mendive.tapia@ub.edu"
+            "access_right_of_data": {
+                "default": "Creative Commons Attribution 4.0 International",
+                "description": "See file ./LICENSE "
+            },
+            "access_right_of_python_code": {
+                "default": "MIT License",
+                "description": "See file ./hdf5_and_metadata_class/LICENSE"
             },
             "contributors": {
-                "description": "Contributors",
+                "description": "Main contributor",
                 "items": {
                     "properties": {
                         "affiliation": {
@@ -32,6 +36,10 @@ class metadata_general:
                         "name": {
                             "description": "Full name of person or organisation. Personal name format: family, given.",
                             "type": "Mendive Tapia, Eduardo"
+                        },
+                        "email": {
+                            "description": "email address.",
+                            "type": "e.mendive.tapia@ub.edu"
                         },
                         "orcid": {
                             "description": "ORCID identifier for creator.",
@@ -49,12 +57,23 @@ class metadata_general:
                     }
                 }
             },
+            "key words" : {
+                "description": "key words for the identification of the interest of this repository",
+                "enum": [
+                    "Magnetism and magnetic materials",
+                    "Caloric effects",
+                    "ab initio thermodynamics",
+                    "La(Fe,Si)13 compounds",
+                    "Density Functional Theory",
+                    "disordered local moment (DLM) theory"
+                ]
+            },
             "target communities" : {
                 "description": "scientific communities and research areas with potential interest in this work",
                 "enum": [
                     "Caloric refrigeration",
                     "Magnetism and magnetic materials",
-                    "Density Functional theory calculations"
+                    "Density Functional Theory"
                 ]
             },
         "description": {
@@ -62,11 +81,14 @@ class metadata_general:
         "type": "La(FexSi1−x)13 and derived quaternary compounds are well-known for their giant, tunable, magneto- and barocaloric responses around a first-order paramagnetic-ferromagnetic transition near room temperature with low hysteresis. Remarkably, such a transition shows a large spontaneous volume change together with itinerant electron metamagnetic features. While magnetovolume effects are well-established mechanisms driving first-order transitions, purely electronic sources have a long, subtle history and remain poorly understood. Here we apply a disordered local moment picture to quantify electronic and magnetoelastic effects at finite temperature in La(FexSi1−x)13 from first-principles. We obtain results in very good agreement with experiment and demonstrate that the magnetoelastic coupling drives the first-order character and causes at the same time a huge electronic entropy contribution to the caloric response."
         },
         "doi": {
-        "description": "Digital Object Identifier (DOI).",
+        "description": "Digital Object Identifier (DOI) of publication.",
         "type": "https://doi.org/10.48550/arXiv.2302.06484"
         },
         "grants": {
-        "description": "MSCA-IF"
+        "description": "MSCA-IF",
+        "project acronym": "MULTICALORICS",
+        "project number": "101025767",
+        "project title": "Multicaloric refrigeration enhanced by multisite interactions: Bridging theory and experiment"
         }
         }
         return
